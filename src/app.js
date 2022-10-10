4b3b72adef2b35ced7b2cfd67722f46ededd0dd0
garvittyagi <garvittyagi99@gmail.com>
2022-10-10 10:12:47 +0530

    Prograd Assignment

--- src/app.js
+++ src/app.js
@@ -3,14 +3,38 @@
 //Debug the type of data provided
 //Return the types concatenated in a single variable
 
+function moreAboutHome(address, distanceFromTown, hasNeighbours)
+{
+ var res=(typeof address)+(typeof distanceFromTown)+(typeof hasNeighbours);
+ return res;
+}
+
+
 //Progression 2:
 //Check if the data given is of the right type
 //parents = String, noOfSiblings = Number, isNuclearFamily = Boolean
+
+function moreAboutKaren(parents, noOfSiblings, isNuclearFamily)
+{
+    if((typeof parents === "string") && (typeof noOfSiblings === "number") && (typeof isNuclearFamily === "boolean"))
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
 
 //Progression 3:
 //Lily is suspicious about Karen's new friend
 //Karen tells her friend's age and even writes it down
 //Check which one those is not a number (NaN) and return that value
+
+const doesFriendExist = (ageInText, ageInNumber) => {
+    return isNaN(ageInText) ? ageInText : ageInNumber;
+}
+
 
 //Progression 4:
 //Lily gave Karen x sweets
@@ -19,9 +43,32 @@
 //Her friend divided the remaining sweets into 2 parts for each
 //How many sweets did her friend get to eat?
 
+const sweetTooth = (totalNoOfSweets, sweetsConsumedByKaren, sweetsConsumedInNMeters, metersToTravel) => 
+{
+    if (typeof totalNoOfSweets === "number" && typeof sweetsConsumedByKaren === "number" && typeof sweetsConsumedInNMeters === "number" && typeof metersToTravel === "number") 
+    {
+        const remaining = totalNoOfSweets - sweetsConsumedByKaren;
+        const newsweetsEaten = metersToTravel * sweetsConsumedInNMeters;
+        return (remaining - newsweetsEaten) / 2;
+    } 
+    else 
+    {
+        return "No sweets for Karen's friend";
+    }
+}
+
 //Progression 5:
 //As Lily moves closer, it gets colder. She checks the temperature on her mobile
 //It only shows in fahrenheit. Convert the data to celsius and return it.
+
+const convertToCelsius = (fahrenheit) => 
+{
+    if (typeof fahrenheit !== "number") 
+    {
+        return "Technical Error!";
+    }
+    return (fahrenheit - 32) * 5 / 9;
+}
 
 //Progression 6:
 //Lily can now do multiple things to deal with this
@@ -30,6 +77,25 @@
 //3. Counsel her daughter herself
 //4. Lock her daughter in her room
 //Given a value, return which of these above actions Lily would take
+
+const aDifficultChoice = (choice) => {
+    if (choice == 1)
+     {
+      return "Take her daughter to a doctor"
+    } 
+    else if (choice == -1) 
+    {
+      return "Break down and give up all hope"
+    } 
+    else if (choice == "I give up") 
+    {
+      return "Refused to do anything for Karen"
+    } 
+    else 
+    {
+      return "Wasn't able to decide"
+    }
+  }
 
 //Progression 7:
 //Lily realized that she'd hurt her daughter
